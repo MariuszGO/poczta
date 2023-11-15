b4c5e58daa4a86651976534cdd794b1deddc1872
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2023-11-15 23:48:36 +0100

    n

--- poczta/MainWindow.xaml.cs
+++ poczta/MainWindow.xaml.cs
@@ -29,5 +29,57 @@ namespace poczta
         {
 
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Uri postcardImg = new Uri("list.png", UriKind.Relative);
+            Uri letterImg = new Uri("list1.png", UriKind.Relative);
+            Uri packageImg = new Uri("list2.png", UriKind.Relative);
+
+            if (pocztowka.IsChecked == true)
+            {
+                cena.Text = "Cena: 1zł";
+                zdj.Source = new BitmapImage(postcardImg);
+            }
+            if (list.IsChecked == true)
+            {
+                cena.Text = "Cena: 1,5zł";
+                zdj.Source = new BitmapImage(letterImg);
+            }
+            if (paczka.IsChecked == true)
+            {
+                cena.Text = "Cena: 10zł";
+                zdj.Source = new BitmapImage(packageImg);
+            }
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string postalCode = kod.Text;
+            if (postalCode.Length == 5)
+            {
+                try
+                {
+                    int numericPostalCode = Convert.ToInt32(kod.Text);
+                    MessageBox.Show("Dane prawidłowe");
+                }
+                catch (System.FormatException)
+                {
+                    MessageBox.Show("W kodzie pocztowym powinny być tylko cyfry");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nieprawidłowa długość kodu pocztowego!");
+            }
+
+        }
+
+        private void list_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
+    
 }
